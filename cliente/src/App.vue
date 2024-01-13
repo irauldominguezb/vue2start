232e0e29b9d53c2e4f48a11fb3f7479fa590fd10
Raúl Domínguez <105244391+irauldominguezb@users.noreply.github.com>
2024-01-12 22:55:56 -0600

    commit
    hola

---
+++ cliente/src/App.vue
@@ -0,0 +1,87 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="header">
+      <b-navbar variant="faded" type="light">
+            <b-navbar-brand tag="h1" class="m-2 text-white text-center">Practica 1</b-navbar-brand>
+          </b-navbar>
+    </div>
+    <div class="sidebar"></div>
+    <div class="content">
+      <div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+        <div class="box">
+          <b-card bg-variant="light" text-variant="secondary" header="Primary" class="text-center">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </div>  
+</template>
+
+<style scoped>
+
+.header{
+  background-color: #042b61;
+  float: left;
+  height: 76px;
+  width: 100%;
+}
+
+.sidebar{
+  float: left;
+  width: 13%;
+  height: 699px;
+  background-color: #049474;
+}
+
+.content{
+  width: 87%;
+  background-color: #f2f2f2;
+  height: 699px;
+  float: left;
+}
+.box{
+  width: 250px;
+  height: 150px;
+  margin: 20px 10px 10px 20px;
+  float: left;
+}
+</style>
